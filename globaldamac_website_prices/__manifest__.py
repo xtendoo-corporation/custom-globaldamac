516f5e5a143547ad7d14511042d7ddaff40a7e20
GonzalezJimenezSalvador <salvador.gon.jim@gmail.com>
2024-12-27 09:55:04 +0100

    hide prices on web

---
+++ globaldamac_website_prices/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2024 Salvador Aramis González Jiménez(https://xtendoo.es)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Globaldamac Website Prices",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Salvador Aramis González Jiménez, Xtendoo",
+    "category": "Website",
+    "depends":
+        [
+            "website_sale", "base", "website", "sale",
+        ],
+    "data":
+        [
+            "views/templates.xml",
+        ],
+    'installable': True,
+    'active': False,
+}
