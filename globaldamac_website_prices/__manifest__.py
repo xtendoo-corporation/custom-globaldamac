c16929d80ba3488acc3bf77511d7df01de2a04e2
guillermo2005200 <91117754+guillermo2005200@users.noreply.github.com>
2025-04-08 08:59:38 +0200

    17.0

--- globaldamac_website_prices/__manifest__.py
+++ globaldamac_website_prices/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Globaldamac Website Prices",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "license": "AGPL-3",
     "author": "Salvador Aramis González Jiménez, Xtendoo",
     "category": "Website",
