39871a75ccaad702943ddb8da0bf48ecf931d068
Gokac9 <theabraham9@gmail.com>
2026-09-22 12:36:11 +0200

    add modulo globaldamac_custom_report

---
+++ globaldamac_custom_report/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2026 Xtendoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Globaldamac Custom Report",
+    "version": "17.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Xtendoo",
+    "category": "Sales",
+    "depends":
+        [
+            "sale",
+        ],
+    "data":
+        [
+            "report/report_saleorder_document.xml",
+        ],
+    'installable': True,
+    'active': True,
+}
